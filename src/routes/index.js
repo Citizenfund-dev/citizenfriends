df6f9c4c134839022fdd04e07189a39481c3b2e8
ju-none <julien.leclercqprof@gmail.com>
2026-08-27 16:31:28 +0200

    fix + update sitempa & robot

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 export { default as Home } from "./Home";
 export { default as NotFound } from "./404";
 export { default as Bruxelles } from "./Bruxelles";
-export { default as Liege } from "./Liège";
+export { default as Liège } from "./Liege";
 export { default as BrabantWallon } from "./Brabant-Wallon";
